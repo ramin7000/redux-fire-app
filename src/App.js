421baa2d38933534b560be362b70743cbecee4f7
Ramin Lohrasbi <mr.ramin7000@gmail.com>
2022-05-06 05:36:32 -0700

    Set SignIn and SignUp

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 
 
 function App() {
+
+
   return (
       <BrowserRouter>
           <div className="App">
@@ -17,6 +21,8 @@ function App() {
               <Routes>
                   <Route path="/" element={<Dashboard />} />
                   <Route path="/project/:id" element={<ProjectDetails />} />
+                  <Route path="/signin" element={<SignIn />} />
+                  <Route path="/signup" element={<SignUp />} />
               </Routes>
 
               {/*
